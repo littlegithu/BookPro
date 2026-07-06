c83a79cf3e4303100758f9e32cb2e2ec3aa6057b
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-06 14:15:50 +0300

    feat: add dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div className="p-4">
-        <section className="p-4">
+    <div className="p-4 flex flex-col gap-4 min-h-screen">
+        <section className="p-4 flex flex-col gap-2">
             <h1 className="text-3xl font-bold">Welcome, Ian!</h1>
             <h5 classname="text-xl">You have 2 appointments today.</h5>
+        </section>
+
+        <section className="flex flex-row gap-4 p-4">
+            <section className="flex flex-col gap-2">
+                <h2 className="text-2xl font-semibold">Upcoming Appointments</h2>
+                <div className="flex flex-row gap-2"></div>
+            </section>
         </section>
     </div>
   )
