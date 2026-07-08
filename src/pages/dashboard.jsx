d5e761d59158d48f46d758a1200bde780a38b7f5
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-08 14:22:51 +0300

    feat: add dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,18 +1,35 @@
+import { Search } from 'lucide-react'
 import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div className="p-4 flex flex-col gap-4 min-h-screen">
+    <div className="p-4 flex flex-col gap-4 min-h-screen w-full">
         <section className="p-4 flex flex-col gap-2">
-            <h1 className="text-3xl font-bold">Welcome, Ian!</h1>
-            <h5 classname="text-xl">You have 2 appointments today.</h5>
+            <div className="flex flex-col h-full w-10">
+                <h1 className="text-3xl font-bold">Welcome, Ian!</h1>
+                <h5 className="text-xl">You have 2 appointments today.</h5>
+            </div>
+            <div className="flex flex-row gap-2"></div>
         </section>
 
         <section className="flex flex-row gap-4 p-4">
-            <section className="flex flex-col gap-2">
-                <h2 className="text-2xl font-semibold">Upcoming Appointments</h2>
-                <div className="flex flex-row gap-2"></div>
-            </section>
+            <div className="flex flex-col gap-2">
+                <div className="bg-green-400 w-3 h-3 rounded-sm"></div>
+                <h2 className="text-2xl font-semibold">2</h2>
+                <h6 className="text-2xl">Upcoming Appointments</h6>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <div className="bg-gray-400 w-3 h-3 rounded-sm"></div>
+                <h2 className="text-2xl font-semibold">4</h2>
+                <h6 className="text-2xl">Completed Visits</h6>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <div className="bg-gray-400 w-3 h-3 rounded-sm"></div>
+                <h2 className="text-2xl font-semibold">3</h2>
+                <h6 className="text-2xl">Medical records</h6>
+            </div>
         </section>
     </div>
   )
