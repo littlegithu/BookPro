bee96175052a6f042f936e3950b5240477b722e2
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-09 17:49:16 +0300

    feat: add navbar

--- src/components/ui/app/navbar.jsx
+++ src/components/ui/app/navbar.jsx
@@ -4,10 +4,10 @@ import { Button } from '../button'
 export default function Navbar() {
   return (
     <div className="">
-        <Button className="text-[14px] text-[#4a6278] border-none rounded-[7px] px-2 py-3.5 hover:bg-[#e6f4f2] hover:text-[#0f7b6c] cursor-pointer">Browse Doctors</Button>
-        <Button className="text-[14px] text-[#4a6278] border-none rounded-[7px] px-2 py-3.5 hover:bg-[#e6f4f2] hover:text-[#0f7b6c] cursor-pointer">About</Button>
-        <Button className="text-[14px] text-[#4a6278] border-none rounded-[7px] px-2 py-3.5 hover:bg-[#e6f4f2] hover:text-[#0f7b6c] cursor-pointer">Login</Button>
-        <Button className="text-[14px] font-semibold text-white border-none rounded-[7px] px-2.5 py-5 bg-[#0f7b6c] cursor-pointer">Get Started</Button>
+        <Button className="text-[14px] text-(--slate) border-none rounded-[7px] px-2 py-3.5 hover:bg-(--teal-light) hover:text-(--teal) cursor-pointer">Browse Doctors</Button>
+        <Button className="text-[14px] text-(--slate) border-none rounded-[7px] px-2 py-3.5 hover:bg-(--teal-light) hover:text-(--teal) cursor-pointer">About</Button>
+        <Button className="text-[14px] text-(--slate) border-none rounded-[7px] px-2 py-3.5 hover:bg-(--teal-light) hover:text-(--teal) cursor-pointer">Login</Button>
+        <Button className="text-[14px] font-semibold text-white border-none rounded-[7px] px-2.5 py-5 bg-(--teal) cursor-pointer">Get Started</Button>
     </div>
   )
 }
