ae0dfb3c520a6e52434ae5090be3d3d9fabed904
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-09 17:53:59 +0300

    feat: add navbar

--- src/components/ui/app/navbar.jsx
+++ src/components/ui/app/navbar.jsx
@@ -3,7 +3,7 @@ import { Button } from '../button'
 
 export default function Navbar() {
   return (
-    <div className="">
+    <div className="bg-card flex flex-row items-center justify-between border-b border-border py-15 h-17">
         <Button className="text-[14px] text-(--slate) border-none rounded-[7px] px-2 py-3.5 hover:bg-(--teal-light) hover:text-(--teal) cursor-pointer">Browse Doctors</Button>
         <Button className="text-[14px] text-(--slate) border-none rounded-[7px] px-2 py-3.5 hover:bg-(--teal-light) hover:text-(--teal) cursor-pointer">About</Button>
         <Button className="text-[14px] text-(--slate) border-none rounded-[7px] px-2 py-3.5 hover:bg-(--teal-light) hover:text-(--teal) cursor-pointer">Login</Button>
