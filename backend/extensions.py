47c9929b8f5320cf5de2c9fee9b3eaa5190bfb11
risper njeri <rispernjeri789@gmail.com>
2026-07-30 21:29:34 +0300

    add changes

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,8 +1,8 @@
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 from sqlalchemy import MetaData
 
 metadata = MetaData(
